ba3eec218252597a1c4e09d6ddb97a2df2f44b99
Damola <adebitandamola02@gmail.com>
2024-11-30 21:41:32 +0100

    format code structure

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ const number = document.querySelector(".adviceNo");
 const diceBtn = document.querySelector(".dice-container");
 
 async function fetchData() {
-    try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        const quizData = await response.json();
-        adviceText.textContent = `"${quizData.slip.advice}"`;
-        number.textContent = `Advice #${quizData.slip.id}`;
-        console.log(quizData);
-    } catch (error) {
-        console.error();
-    }
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const quizData = await response.json();
+    adviceText.textContent = `"${quizData.slip.advice}"`;
+    number.textContent = `Advice #${quizData.slip.id}`;
+    console.log(quizData);
+  } catch (error) {
+    console.error();
+  }
 }
 
 diceBtn.addEventListener("click", () => {
-    fetchData();
-})
-
-
+  fetchData();
+});
 
 fetchData();
