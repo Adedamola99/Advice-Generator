d1cad10649c8e5b0a8ecbb7849b054cc53db35df
Damola <adebitandamola02@gmail.com>
2025-01-28 10:34:59 +0100

    improved advice app

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const adviceText = document.querySelector(".text");
 const number = document.querySelector(".adviceNo");
 const diceBtn = document.querySelector(".dice-container");
+let errorMessage;
 
 async function fetchData() {
+  diceBtn.classList.add("animate");
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
     const quizData = await response.json();
     adviceText.textContent = `"${quizData.slip.advice}"`;
     number.textContent = `Advice #${quizData.slip.id}`;
-    console.log(quizData);
+    diceBtn.classList.remove("animate");
   } catch (error) {
-    console.error();
+    diceBtn.classList.remove("animate");
+    adviceText.textContent = error.message;
+    console.error(`Error fetching data: ${error.message}`);
   }
 }
 
